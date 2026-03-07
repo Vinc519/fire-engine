33951abaa1e80da7019d414491eb7212246a15b9
Vinc519 <gagalegoth@gmail.com>
2026-03-07 15:54:14 +0100

    Create sandbox folder and the main file

---
+++ sandbox/main.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+// Entry point for the Fire Engine sandbox.
+// Use this file to manually test engine features during development.
+
+int main()
+{
+    printf("Fire Engine - Sandbox\n");
+    return 0;
+}
