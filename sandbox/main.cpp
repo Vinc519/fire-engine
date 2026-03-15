f8933a124ec53f28f6fa810daaaed7a587bd6baa
Vinc519 <gagalegoth@gmail.com>
2026-03-15 15:24:51 +0100

    Switch spaces for tabulations

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -1,10 +1,9 @@
 #include <cstdio>
 
 // Entry point for the Fire Engine sandbox.
-// Use this file to manually test engine features during development.
 
 int main()
 {
-    printf("Fire Engine - Sandbox\n");
-    return 0;
+	printf("Fire Engine - Sandbox\n");
+	return 0;
 }
