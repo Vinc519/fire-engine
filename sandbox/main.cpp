5d1692b55deea03a799c90fa12e6ce8604b53a9b
PINGAUX Aymeric <pingaux@et.esiea.fr>
2026-05-02 12:15:38 +0200

    Test the logger

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -1,9 +1,15 @@
-#include <cstdio>
+#include <core/debug/Logger.h>
 
+#include <cstdio>
 // Entry point for the Fire Engine sandbox.
 
 int main()
 {
+	fe::Logger::Init("log.txt");
+
 	printf("Fire Engine - Sandbox\n");
+	FE_LOG_INFO("Print message :%d", 8);
+
+	fe::Logger::Shutdown();
 	return 0;
 }
