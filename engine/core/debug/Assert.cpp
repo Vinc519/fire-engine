7053828c1d7e64ae1d93d93b55a2e7f62b05cb3c
PINGAUX Aymeric <gagalegoth@gmail.com>
2026-06-27 12:26:46 +0200

    Change the printed string to begin at the fire-engine directory

--- engine/core/debug/Assert.cpp
+++ engine/core/debug/Assert.cpp
@@ -1,6 +1,14 @@
 #include "Assert.h"
-#include <cstdio>
 
-void FE_AssertReport(const char *expr, const char *msg, const char *file, i32 line) {
-  fprintf(stderr, "[ASSERT] %s\n  Expression : %s\n  File : %s:%d\n", msg, expr, file, line);
+#include <cstdio>
+#include <cstring>
+
+void FE_AssertReport(const char *expr, const char *msg, const char *file, i32 line)
+{
+	fprintf(stderr, "[ASSERT] %s\n  Expression : %s\n  File : %s:%d\n", 
+			msg,
+			expr,
+	        strstr(file, "fire-engine") + 12,
+			line
+	);
 }
